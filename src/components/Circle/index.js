3a69369b5b2a955df3a1d2f7617d3e5326dc1407
Andrew Feng <afeng@biospective.com>
2017-12-05 13:52:02 -0500

    implemented circle component

---
+++ src/components/Circle/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+
+const Circle = ({text, url}) => {
+  return (
+    <div className="circle">
+      <h4>{text}</h4>
+    </div>
+  )
+}
+
+export default Circle
